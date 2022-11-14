af8b078d4af39fcfce991e7616d14a62b151c2c9
Miguel Arcanjo <945507756m@gmail.com>
2022-11-14 16:50:29 -0300

    não é possivel add valor negativo em dinheiro/cartão

--- src/menu_opcoes/Despesa.java
+++ src/menu_opcoes/Despesa.java
@@ -7,31 +7,41 @@ public class Despesa {
 		Scanner scanner = new Scanner(System.in);
 		double rendaMes = 0, tudoEmDinheiro = 0, tudoEmCartao = 0, totalDespesas = 0, restante = 0;
 		boolean continuar = false;
-		int opcao;
+		int opcao, teste = 11;
 
 		do {
-			System.out.println("qual a sua renda neste mes: ");
-			rendaMes = scanner.nextDouble();
-			System.out.println("quantidade gasto em DINHEIRO: ");
-			tudoEmDinheiro = scanner.nextDouble();
-			System.out.println("quantidade gasto em CARTÃO: ");
-			tudoEmCartao = scanner.nextDouble();
-			System.out.println("");
-
-			totalDespesas = tudoEmDinheiro + tudoEmCartao;
-			System.out.println("Total de despesas");
-			System.out.println(totalDespesas + " \n");
-
-			restante = rendaMes - totalDespesas;
-
-			if (totalDespesas > rendaMes) {
-				System.out.println("Fazer empréstimo no banco \nVoce ficou com: R$" + restante);
-			} else if (restante == 0) {
-				System.out.println("Saldo: R$" + restante + "\nNão restou nenhum dinheiro");
-			} else {
-				System.out.println("Você ficou com o saldo positivo neste mês \nRestou em sua conta R$" + restante);
+			do {
+				System.out.println("qual a sua renda neste mes: ");
+				rendaMes = scanner.nextDouble();
+				System.out.println("quantidade gasto em DINHEIRO: ");
+				tudoEmDinheiro = scanner.nextDouble();
+				System.out.println("quantidade gasto em CARTÃO: ");
+				tudoEmCartao = scanner.nextDouble();
 				System.out.println("");
-			}
+
+				if (tudoEmDinheiro < 0 || tudoEmCartao < 0) { // caso o que for gasto em dinheiro ou cartao for negativo, refazer todo os passos
+					System.out.println("Não é possível inserir valores negativos neste campo");
+					System.out.println("Insira novamente");
+					continuar = true;
+					break;
+				} else {
+					totalDespesas = tudoEmDinheiro + tudoEmCartao;
+					System.out.println("Total de despesas");
+					System.out.println(totalDespesas + " \n");
+
+					restante = rendaMes - totalDespesas;
+
+					if (totalDespesas > rendaMes) {
+						System.out.println("Fazer empréstimo no banco \nVoce ficou com: R$" + restante);
+					} else if (restante == 0) {
+						System.out.println("Saldo: R$" + restante + "\nNão restou nenhum dinheiro");
+					} else {
+						System.out.println(
+								"Você ficou com o saldo positivo neste mês \nRestou em sua conta R$" + restante);
+						System.out.println("");
+					}
+				}
+			} while (teste < 10);
 
 			System.out.println("\nDeseja calcular outra despesa? \n1 - Sim ou 2 - Não");
 			opcao = scanner.nextInt();
@@ -40,6 +50,7 @@ public class Despesa {
 			} else {
 				continuar = false;
 			}
+
 		} while (continuar);
 	}
 }
