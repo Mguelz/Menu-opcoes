334afa2a3bf65983b6d61002039aa6c9968d1d8b
Miguel <945507756m@gmail.com>
2023-04-18 15:57:49 -0300

    tirando todos os sysouts da classe Conversorfahrenheit

--- src/menu_opcoes/calculo/ConversorFahrenheit.java
+++ src/menu_opcoes/calculo/ConversorFahrenheit.java
@@ -1,33 +1,36 @@
 package menu_opcoes.calculo;
 
 import java.text.DecimalFormat;
+
 import menu_opcoes.LeitorDeDados;
+import menu_opcoes.Mensagem;
 
 public class ConversorFahrenheit {
-	
+
 	/**
-	 * Irá converter de a temperatura de Fahrenheit para Celcius 
+	 * Irá converter de a temperatura de Fahrenheit para Celcius
 	 */
 	public static void ConverterFahrenheit() {
 		LeitorDeDados scanner = new LeitorDeDados();
+		Mensagem m = new Mensagem();
 
 		double celcius, fahrenheit;
 		boolean continuar = false;
 		int opcao = 0;
 
-		System.out.println("\n------------------ CONVERSOR DE GRAUS -----------------");
-		System.out.println("Converter a temperatura em Fahrenheit para Celcius \n");
+		m.lnPrintaMensagem("------------------ CONVERSOR DE GRAUS -----------------");
+		m.printaMensagemLn("Converter a temperatura em Fahrenheit para Celcius");
 
 		do {
 			DecimalFormat formatador = new DecimalFormat("#0.0");
-			System.out.print("Digite a temperatura em fahrenheit: ");
+			m.lnPrintaMensagemLn("Digite a temperatuda em fahrenheit: ");
 			fahrenheit = scanner.pegarNumeroQuebrado();
 
 			celcius = (5 * (fahrenheit - 32)) / 9;
 
-			System.out.println("Temperatura em Celsius: " + formatador.format(celcius) + "°C");
-
-			System.out.println("Deseja converter novamente? \n1 - Sim ou 2 - Não");
+			m.printaMensagemLn("Temperatura em Celsious: " + formatador.format(celcius) + "ºC");
+			m.printaMensagemLn("Deseja converter novamente?");
+			m.printaMensagemErroLn("1 - Sim ou  2 - Não");
 			opcao = scanner.pegarNumeroInteiro();
 			if (opcao == 1) {
 				continuar = true;
