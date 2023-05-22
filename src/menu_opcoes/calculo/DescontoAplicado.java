7a69e4ca6fa9a6536f43b579f323fa298cc0a4f0
Miguel <945507756m@gmail.com>
2023-05-22 19:05:41 -0300

    refactor: tirando sysouts da classe Frete

--- src/menu_opcoes/calculo/DescontoAplicado.java
+++ src/menu_opcoes/calculo/DescontoAplicado.java
@@ -1,43 +1,47 @@
 package menu_opcoes.calculo;
 
 import menu_opcoes.LeitorDeDados;
+import menu_opcoes.Mensagem;
 
 public class DescontoAplicado {
-	
+
 	/**
-	 * de acordo com a quantidade que for gasta o sistema irá calcular o desconto 
+	 * de acordo com a quantidade que for gasta o sistema irá calcular o desconto
 	 */
 	public static void desontoAplicado() {
 		LeitorDeDados scanner = new LeitorDeDados();
+		Mensagem m = new Mensagem();
+
 		double desconto, valorCompra, valorDescontado;
 		boolean continuarComprando = true;
 		int opcao;
 
-		System.out.println("\n------------------ DESCONTO APLICADO -----------------");
+		m.lnPrintaMensagem("------------------ DESCONTO APLICADO -----------------");
 		do {
-			System.out.println("\nDigite o valor da sua compra: ");
+			m.lnPrintaMensagemSemPularLinha("Digite o valor da sua compra: ");
 			valorCompra = scanner.pegarNumeroQuebrado();
 
 			if (valorCompra <= 200) {
 				desconto = valorCompra * 0.04;
 				valorDescontado = valorCompra - desconto;
-				System.out.print("\nSeu desconto foi de " + desconto);
-				System.out.println(" | desconto em porcentagem = 4%");
-				System.out.println("Valor total R$: " + valorDescontado);
+				m.lnPrintaMensagem("Seu desconto foi de " + desconto);
+				m.printaMensagem(" | desconto em porcentagem = 4%");
+				m.printaMensagem("Valor total R$: " + valorDescontado);
 			} else if (valorCompra <= 500) {
 				desconto = valorCompra * 0.06;
 				valorDescontado = valorCompra - desconto;
-				System.out.print("\nSeu desconto foi de " + desconto);
-				System.out.println(" | desconto em porcentagem = 6%");
-				System.out.println("Valor total R$: " + valorDescontado);
+				m.printaMensagemSemPularLinha("Seu desconto foi de " + desconto);
+				m.printaMensagem(" | desconto em porcentagem = 6%");
+				m.printaMensagem("Valor total R$: " + valorDescontado);
 			} else {
 				desconto = valorCompra * 0.08;
 				valorDescontado = valorCompra - desconto;
-				System.out.print("\nSeu desconto foi de " + desconto);
-				System.out.println(" | desconto em porcentagem = 8%");
-				System.out.println("Valor total R$: " + valorDescontado);
+				m.lnPrintaMensagemSemPularLinha("Seu desconto foi de " + desconto);
+				m.printaMensagem(" | desconto em porcentagem = 8%");
+				m.printaMensagem("Valor total R$: " + valorDescontado);
 			}
-			System.out.println("\nDeseja calcular outra despesa? \n1 - Sim ou 2 - Não");
+			m.lnPrintaMensagem("Deseja calcular outra despesa?");
+			m.printaMensagem("1 - Sim ou 2 - Não");
 			opcao = scanner.pegarNumeroInteiro();
 			if (opcao == 2) {
 				continuarComprando = false;
