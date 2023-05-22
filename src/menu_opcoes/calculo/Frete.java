c9568d82b109cf12c41bf02531efb0e0f4b0e49b
Miguel <945507756m@gmail.com>
2023-05-22 19:33:14 -0300

    refactoring: tratamento de erro

--- src/menu_opcoes/calculo/Frete.java
+++ src/menu_opcoes/calculo/Frete.java
@@ -13,38 +13,47 @@ public class Frete {
 		LeitorDeDados scanner = new LeitorDeDados();
 		Mensagem m = new Mensagem();
 
+		String uf;
 		boolean continuar = true;
+		byte digitado = 0;
 
 		m.lnPrintaMensagemLn("------------------ FRETE -----------------");
 		do {
-			m.printaMensagem("Digite sua UF");
-			String uf = scanner.pegarTextoCurto().toUpperCase();
-			switch (uf) {
-			case "SP":
-			case "RJ":
-				m.printaMensagem("Seu desconto é de R$ 10,00");
-				break;
-			case "AM":
-				m.printaMensagem("Seu desconto é de R$ 35,00");
-				break;
-			case "GO":
-				m.printaMensagem("Seu desconto é de R$ 30,00");
-				break;
-			case "BA":
-				m.printaMensagem("Seu desconto é de R$ 50,00");
-				break;
-			default:
-				m.printaMensagem("Sua UF não possui desconto");
-				break;
-			}
-			m.lnPrintaMensagemLn("Deseja calcular outro IMC?");
-			m.lnPrintaMensagemLn("1 - SIM ou 2 - NÃO");
-			uf = scanner.pegarTextoCompleto(); // TODO o programa nao esta parando aqui
-			
-			if (uf.equals(1)) {
+			try {
+				m.printaMensagemSemPularLinha("Digite sua UF: ");
+				uf = scanner.pegarTextoCurto().toUpperCase();
+				switch (uf) {
+				case "SP":
+				case "RJ":
+					m.printaMensagem("Seu desconto é de R$ 10,00");
+					break;
+				case "AM":
+					m.printaMensagem("Seu desconto é de R$ 35,00");
+					break;
+				case "GO":
+					m.printaMensagem("Seu desconto é de R$ 30,00");
+					break;
+				case "BA":
+					m.printaMensagem("Seu desconto é de R$ 50,00");
+					break;
+				default:
+					m.printaMensagem("Sua UF não possui desconto");
+					break;
+				}
+				m.lnPrintaMensagem("Deseja calcular outro IMC?");
+				m.printaMensagemLn("1 - SIM ou 2 - NÃO");
+				digitado = scanner.pegarByteDigitado(); scanner.pegarTextoCompleto(); // TODO o programa nao esta parando aqui
+
+				if (digitado == 1) {
+					continuar = true;
+				} else {
+					continuar = false;
+				}
+
+			} catch (NumberFormatException e) {
+				m.lnPrintaMensagemLn("É somente permitido letras");
+				m.printaMensagemErro("Message error" + e);
 				continuar = true;
-			} else {
-				continuar = false;
 			}
 		} while (continuar);
 	}
